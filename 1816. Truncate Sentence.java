3434970a6099f94e5447b93a4e137872f5e9cd9e
KOLLI SRI KRISHNA KARTHIKEYA <107982190+KARTHIK174@users.noreply.github.com>
2024-02-04 10:27:52 +0530

    1816. Truncate Sentence.java

---
+++ 1816. Truncate Sentence.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        int count =0,i=0;
+        for(i=0;i<s.length();i++){
+            if(s.charAt(i)==' '){
+                count++;
+            }
+            if(count==k) break;
+        }
+        if(i==s.length()) return s;
+        return s.substring(0,i);
+    }
+}
